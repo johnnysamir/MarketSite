270257c4bd68469b44dd927af3402fe2e8cff0dc
johnnysamir <69882226+johnnysamir@users.noreply.github.com>
2026-07-18 11:56:34 -0300

    Agregados enlaces a Instagram y WhatsApp en el Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,16 @@ export const Footer = () => {
             <p>Sitio desarrollado por JVargas</p>
             <nav>
                 <ul className="nav-list">
-                    <li>Whatsapp</li>
-                    <li>Instagram</li>
+                    <li>
+                        <a href="https://wa.me/5491121800021" target="_blank" rel="noopener noreferrer">
+                            WhatsApp
+                        </a>
+                    </li>
+                    <li>
+                        <a href="https://www.instagram.com/sys.impresioness/" target="_blank" rel="noopener noreferrer">
+                            Instagram
+                        </a>
+                    </li>
                 </ul>
             </nav>
         </footer>
